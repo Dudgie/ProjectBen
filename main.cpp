2d62c16d622d2a87f1292c4e92a044c14d4f5952
Ben Dudgeon <ben@benmac.home>
2014-09-24 20:48:21 +0100

    changed files on project

---
+++ main.cpp
@@ -0,0 +1,82 @@
+//
+//  main.cpp
+//  Balancing Table
+//
+//  Created by Ben Dudgeon on 29/08/2014.
+//  Copyright (c) 2014 BenProject. All rights reserved.
+//
+
+#include <iostream>
+#include <opencv/highgui.h>
+#include <opencv/cv.h>
+#include <signal.h>
+#include "TrackObject.cpp"
+#include "PID.cpp"
+
+using namespace cv;
+using namespace std;
+TrackObject track;
+PID controller;
+
+int hMIN = 0; int hMAX = 256;
+int sMIN = 0; int sMAX = 256;
+int vMIN = 0; int vMAX = 256;
+
+void trackIt (int, void*)
+{
+    
+}
+
+void createTrackBars()
+{
+    string trackers = "Trackbars";
+    namedWindow(trackers, 0);
+    
+    createTrackbar("trackHMIN", trackers, &hMIN, hMAX, trackIt);
+    createTrackbar("trackHMAX", trackers, &hMAX, hMAX, trackIt);
+    createTrackbar("trackSMIN", trackers, &sMIN, hMAX, trackIt);
+    createTrackbar("trackSMAX", trackers, &sMAX, hMAX, trackIt);
+    createTrackbar("trackVMIN", trackers, &vMIN, hMAX, trackIt);
+    createTrackbar("trackVMAX", trackers, &vMAX, hMAX, trackIt);
+}
+
+void sig_handler(int sig);
+
+int main(int argc, const char * argv[])
+{
+    bool ctrlCPressed = false;
+    
+    struct sigaction sig_struct;
+    sig_struct.sa_handler = sig_handler;
+    sig_struct.sa_flags = 0;
+    sigemptyset(&sig_struct.sa_mask);
+    
+    if (sigaction(SIGINT, &sig_struct, NULL) == -1)
+    {
+        cout << "Problem with sigaction" << endl;
+        exit (1);
+    }
+    
+    namedWindow("webcamFeed", 1);
+    namedWindow("MorphedBinary", 1);
+    track.getCamera();
+    createTrackBars();
+    while (true)
+    {
+        if (ctrlCPressed)
+        {
+            cout << "Quitting" << endl;
+            exit(0);
+        }
+        track.displayCameraFeed();
+        track.giveValues(hMIN, hMAX, sMIN, sMAX, vMIN, vMAX);
+        track.imageToBinary();
+        track.binaryToXY();
+        track.displayXY();
+        
+        controller.CoOrdinateToDistance(track.getX(), track.getY());
+        
+        waitKey(33);
+    }
+    return 0;
+}
